7a73ec34ce75cb0c19563cdacf7965f3f745ad21
prajktas <88100743+prajktas@users.noreply.github.com>
2021-09-27 16:53:13 +0000

    Add files via upload

---
+++ assg4/ghost/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+provider "docker" {
+  host    = "npipe:////./pipe/docker_engine"
+}
+
+# Download the latest Ghost Image
+resource "docker_image" "ghost_image" {
+  name = "${var.image_name}"
+}
+
+# Start the Container
+resource "docker_container" "ghost_container" {
+  name  = "${var.container_name}"
+  image = "${docker_image.ghost_image.latest}"
+  ports {
+    internal = "2368"
+    external = "${var.ext_port}"
+  }
+}
