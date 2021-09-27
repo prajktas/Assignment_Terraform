83072a79ae2843800a6ab445c228ef68f5acc5ed
prajktas <88100743+prajktas@users.noreply.github.com>
2021-09-27 16:49:06 +0000

    Add files via upload

---
+++ assg2/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+resource "docker_image" "ghost_image" {
+    name = "${var.image_name}"
+}
+
+resource "docker_container" "ghost_container" {
+    name  = "${var.container_name}"
+    image = "${docker_image.ghost_image.latest}"
+    ports {
+        internal = "2368"
+        external = "${var.ext_port}"
+    }
+}
